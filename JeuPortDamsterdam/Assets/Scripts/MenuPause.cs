41b6f871e1584876c6256a4110e0ea23f639e88e
Unknown <manga2000@hotmail.fr>
2019-01-26 17:25:11 +0100

    Health + menu pause

---
+++ JeuPortDamsterdam/Assets/Scripts/MenuPause.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    [SerializeField] private bool gamePause = false;
+
+    [SerializeField] private GameObject pauseMenuPanel;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (gamePause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+   public void Resume()
+    {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1.0f;  
+        gamePause = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0.0f;
+        gamePause = true;                
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
